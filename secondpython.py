3b252f70fa242043f18c5393f07707a376978caf
ven3hub <vin1231@gmail.com>
2025-08-31 11:04:21 +0800

    1st

--- secondpython.py
+++ secondpython.py
@@ -1,2 +1,25 @@
-str = "jervin"
-print(str)
+#str = "jervin"
+#print(str)
+#print("it's really nice using python")
+
+#strings
+first_name = "Jervin"
+last_name = "Santos"
+full_name = first_name + " " + last_name
+print(full_name)
+print(f"hello {full_name}")
+
+foods = ["banana" , "apple" , "lemon"]
+print(f"i love  {foods[1]}")
+print("i love" + foods[2])
+
+#boolean
+
+is_student = False
+
+print(f"are you an student?: {is_student}")
+if is_student:
+    print("You may Enter")
+else:
+    print("dont enter!")
+    
